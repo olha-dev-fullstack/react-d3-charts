e64ea84359a9924c9ba42bb3da95b4f6a8b50571
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-04-16 14:00:20 +0300

    add static line chart

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import LineChart from './charts/LineChart';
+import Histogram from './charts/Histogram'
+import TimeSeries from './charts/TimeSeries';
+function App() {
+
+  return (
+    <div className="App">
+    <h2> React & D3 Chart Examples </h2>
+    <div className='row'>
+      <LineChart width={400} height={300} />
+
+      <TimeSeries width={400} height={300}/>
+
+      <Histogram width={400} height={400}/>
+    </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/charts/Histogram.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Histogram = () => {
+  return (
+    <div>Histogram</div>
+  )
+}
+
+export default Histogram
