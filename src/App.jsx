1e5a1479595b4f3f8b78e3cacbc1dc5722e88e54
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-04-16 14:44:13 +0300

    add histogram and timeseries

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import './App.css'
-import LineChart from './charts/LineChart';
-import Histogram from './charts/Histogram'
-import TimeSeries from './charts/TimeSeries';
+import "./App.css";
+import LineChart from "./charts/LineChart";
+import Histogram from "./charts/Histogram";
+import TimeSeries from "./charts/TimeSeries";
 function App() {
-
   return (
     <div className="App">
-    <h2> React & D3 Chart Examples </h2>
-    <div className='row'>
-      <LineChart width={400} height={300} />
+      <h2> React & D3 Chart Examples </h2>
+      <div className="row">
+        <LineChart width={400} height={300} />
 
-      <TimeSeries width={400} height={300}/>
+        <TimeSeries width={400} height={300} />
 
-      <Histogram width={400} height={400}/>
+        <Histogram width={400} height={400} />
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
